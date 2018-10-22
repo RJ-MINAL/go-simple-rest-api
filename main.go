9390900a1fba97e131d0331a306d33941581fcae
RJ_Minal <rj.minera@gmail.com>
2018-10-21 23:04:11 -0600

    add: Endpoint operations

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 
@@ -22,19 +24,47 @@ type Address struct {
 var people []Person
 
 func getPeopleEndpoint(w http.ResponseWriter, r *http.Request) {
-
+	fmt.Println("Endpoint hit: getPeople")
+	json.NewEncoder(w).Encode(people)
 }
 
 func getPersonEndpoint(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint hit: getPerson")
 
+	params := mux.Vars(r)
+	for _, item := range people {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&Person{})
 }
 
 func createPersonEndpoint(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint hit: createPerson")
 
+	params := mux.Vars(r)
+
+	var person Person
+	_ = json.NewDecoder(r.Body).Decode(&person)
+	person.ID = params["id"]
+
+	people = append(people, person)
+	json.NewEncoder(w).Encode(person)
 }
 
 func deletePersonEndpoint(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint hit: deletePerson")
 
+	params := mux.Vars(r)
+	for index, item := range people {
+		if item.ID == params["id"] {
+			people = append(people[:index], people[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(people)
 }
 
 func main() {
